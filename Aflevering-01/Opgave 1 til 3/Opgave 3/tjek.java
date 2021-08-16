5cb83f279fe362d52bbe385b200766dd2e61f4af
rad1calplays <lucas.hayes@live.dk>
2021-08-16 13:20:06 +0200

    added better explanation to Opgave 3

--- Aflevering-01/Opgave 1 til 3/Opgave 3/tjek.java
+++ Aflevering-01/Opgave 1 til 3/Opgave 3/tjek.java
@@ -8,6 +8,7 @@ public class HelloWorld {
         // System.out.println( 0.3 == 0.1 + 0.1 + 0.1 ); resultere i false
         // hvis man kører System.out.println( 0.1 + 0.1 + 0.1 ); retunere det 0.30000000000000004
         // dette er ikke lig 0.3 og derfor retuneres "false"
-        // dette sker grundet en afrundingsfejl
+        // det er grundet fejlen kendt som "floating point error"
+        // den dannes af måden kommaet bliver indkodet i tallet
      }
 }
